b20c07228c7c87de7c9dd2848671264f25c336dd
marchetype <114883447+marchetype@users.noreply.github.com>
2023-02-07 09:50:38 -0500

    created basic model for Character

--- models/Character.js
+++ models/Character.js
@@ -1,1 +1,37 @@
 const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Character extends Model {}
+
+Character.init(
+    {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        class: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        age: {
+            type: DataTypes.DECIMAL,
+            allowNull: false
+        },
+        height: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        background_color: {
+            type: DataTypes.STRING
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'character',
+    }
+);
+
+module.exports = Character;
