4984003cdd63265448a578c963abeb417636011d
marchetype <114883447+marchetype@users.noreply.github.com>
2023-02-07 18:59:23 -0500

    added foreign key reference within index.js and Character.js

--- models/Class.js
+++ models/Class.js
@@ -1,1 +1,48 @@
 const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Class extends Model {}
+
+Class.init(
+    {
+        class_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        class_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true
+        },
+        strength: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        endurance: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        charisma: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        intelligence: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        agility: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'class',
+    }
+);
+
+module.exports = Class;
