c4613f54963c9beab38e8828b1da7624e5e55e09
bravenbright <bbryantcs@gmail.com>
2023-02-07 20:51:36 -0600

    continued to work on forms.js

--- public/js/forms.js
+++ public/js/forms.js
@@ -8,3 +8,23 @@ const charName = document.getElementById("char-name");
 const charHeight = document.getElementById("char-height");
 const charClass = document.getElementById("char-class");
 const bckgrndColor = document.getElementById("char-background-color")
+
+/* To generate character form I need to grab the id of the entire form and render to page.
+   alternatively grab each individual input id 
+   -- I need to grab the textcontent of the input
+*/
+
+function generateForm() {
+   const createName = {
+      statementQuest: 'Enter Desired Character name'   
+   }
+   document.getElementById 
+}
+
+
+function createCharacter(){
+}
+
+function generateCharacterSheet(){
+
+}
