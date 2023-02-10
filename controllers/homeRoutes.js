ba86f8fa38dc315846887e67144b82788ed13c30
cxlos <bautistac_29@outlook.com>
2023-02-09 19:56:15 -0600

    played around with routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
+//
 router.get('/', (req, res) => {
-    res.render('nav');
+    res.render('homepage');
   });
 
+
+
   module.exports = router;
-  
