db2a1982ec7c1a94088bc4f3fe123e9f34d5170b
Sam O'Cain <samocain93@gmail.com>
2023-02-09 20:28:59 -0600

    controller index updated

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,13 @@
 
-//
 const router = require('express').Router();
+
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
-const userRoutes = require('./api/userRoutes')
 
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-router.use('/user')
+
 
 module.exports = router;
 
