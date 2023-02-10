70ae18f810ccff321e59f30960b9d1e64610037b
bravenbright <bbryantcs@gmail.com>
2023-02-10 01:17:13 -0600

    did not mean to change this

--- models/index.js
+++ models/index.js
@@ -2,23 +2,23 @@ const Character = require('./Character');
 const Class = require('./Class');
 const User = require('./User');
 
-// User.hasMany(Character, {
-//     foreignKey: 'user_id'
-// });
+User.hasMany(Character, {
+    foreignKey: 'user_id'
+});
 
-// Character.belongsTo(User, {
-//     foreignKey: 'user_id',
-//     onDelete: 'CASCADE',
-// })
+Character.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+})
 
-// Character.hasOne(Class, {
-//     foreignKey: 'character_id',
-//     onDelete: 'CASCADE',
-// })
+Character.hasOne(Class, {
+    foreignKey: 'character_id',
+    onDelete: 'CASCADE',
+})
 
-// Class.belongsTo(Character, {
-//     foreignKey: 'character_id',
-//     onDelete: 'CASCADE',
-// })
+Class.belongsTo(Character, {
+    foreignKey: 'character_id',
+    onDelete: 'CASCADE',
+})
 
 module.exports = { Character, Class, User };
