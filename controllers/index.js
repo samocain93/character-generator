4fc6690be65e2cebb91334b40f108e9790c708d6
Sam O'Cain <samocain93@gmail.com>
2023-02-09 20:27:38 -0600

    api index route updated

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,12 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const userRoutes = require('./api/userRoutes')
+
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/user')
 
 module.exports = router;
 
