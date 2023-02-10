3235526ba8f2fdde579b16f643afc5b196e815ee
marchetype <114883447+marchetype@users.noreply.github.com>
2023-02-09 20:27:31 -0500

    revised data within character seeds

--- seeds/character-seeds.js
+++ seeds/character-seeds.js
@@ -4,39 +4,39 @@ const characterData = [
     {
         name: 'John Doe',
         age: 40,
-        height: 1,
+        height: 5.5,
         background_color: 'red',
         class_id: 1,
         user_id: 1
     },
     {
         name: 'Jane Doe',
-        age: 1,
-        height: 1,
+        age: 60,
+        height: 4.5,
         background_color: 'blue',
         class_id: 2,
         user_id: 2
     },
     {
         name: 'Carlos Santana',
-        age: 1,
-        height: 1,
+        age: 75,
+        height: 5.833,
         background_color: 'green',
         class_id: 3,
         user_id: 3
     },
     {
         name: 'Jimi Hendrix',
-        age: 1,
-        height: 1,
+        age: 81,
+        height: 5.833,
         background_color: 'yellow',
         class_id: 4,
         user_id: 4
     },
     {
         name: 'John Lennon',
-        age: 1,
-        height: 1,
+        age: 83,
+        height: 5.833,
         background_color: 'purple',
         class_id: 5,
         user_id: 5
