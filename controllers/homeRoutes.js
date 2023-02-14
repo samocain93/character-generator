d5e39059faaeaa6d468c18d357da7f72b7cdabc6
Adam <asantillana@apple.com>
2023-02-13 20:10:19 -0600

    more code around profile.handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,54 +1,53 @@
-
 //
-const router = require('express').Router();
-const { User } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { User } = require("../models");
+const withAuth = require("../utils/auth");
 
 // Rendering homepage
-router.get('/', (req, res) => {
-    res.render('homepage');
-  });
+router.get("/", (req, res) => {
+  res.render("homepage");
+});
 
 // Rendering homepage with session
-router.get('/login', (req, res) => {
-   if (req.session.logged_in) {
-     res.redirect('/profile');
-     return;
-   }
- 
-   res.render('login');
- });
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
 
-  // Rendering login page
-router.get('/login', async (req, res) => {
-   res.render('login');
+  res.render("login");
 });
 
-router.get('/create', async (req, res) => {
-   res.render('partials/character-form');
+// Rendering login page
+router.get("/login", async (req, res) => {
+  res.render("login");
 });
 
-router.post('/', async (req, res) => {
-   res.render('partials/form', { 'body': req.body });
+router.get("/create", async (req, res) => {
+  res.render("partials/character-form");
 });
 
-router.get('/profile', withAuth, async (req, res) => {
+router.post("/", async (req, res) => {
+  res.render("partials/form", { body: req.body });
+});
 
-   try {
-     const userData = await User.findByPk(req.session.user_id, {
-       attributes: { exclude: ['password'] },
-       include: [{ model: Character }],
-     });
- 
-     const user = userData.get({ plain: true });
- 
-     res.render('profile', {
-       ...user,
-       logged_in: true
-     });
-   } catch (err) {
-     res.status(500).json(err);
-   }
- });
+router.get("/profile", withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Character }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render("profile", {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    console.log("Issue fetching profile");
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
